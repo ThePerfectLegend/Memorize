31b32842f4cfe4903cc4870f8150fbb2e91619ca
Nizami Tagiyev <nizamion24s@gmail.com>
2022-05-28 13:09:47 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Nizami Tagiyev on 28.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
